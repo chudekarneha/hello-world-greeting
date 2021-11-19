71efd543002baa4a7b535856623990562eca4657
NC <94254884+chudekarneha@users.noreply.github.com>
2021-11-19 13:51:29 -0500

    Update GreetingMessage.java

--- src/main/java/GreetingMessage.java
+++ src/main/java/GreetingMessage.java
@@ -14,7 +14,7 @@ public class GreetingMessage {
       if (hours < 12)
       this.message = "Good Morning! \n\n Welcome to CI/CD E2E Cloud Solution";
       else if (hours < 17 && !(hours == 12))
-      this.message = "Good Afternoon! \n\n Welcome to CI/CD E2E Cloud Solution ";
+      this.message = "Good Afternoon! \n\n Welcome to CI/CD E2E Cloud Solution";
       else if (hours == 12)
       this.message = "Good Noon! \n\n Welcome to CI/CD E2E Cloud Solution";
       else
