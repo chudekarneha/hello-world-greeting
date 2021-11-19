9bcc296fab9725059bc95b64fe8017bc8b11de56
NC <94254884+chudekarneha@users.noreply.github.com>
2021-11-19 13:52:12 -0500

    Update GreetingMessageIT.java

--- src/test/java/GreetingMessageIT.java
+++ src/test/java/GreetingMessageIT.java
@@ -16,13 +16,13 @@ public class GreetingMessageIT {
    	GregorianCalendar time = new GregorianCalendar();
  	int hour = time.get(Calendar.HOUR_OF_DAY);
 	if (hour < 12)
-    this.message = "Good Morning!";
+    this.message = "Good Morning! \n\n Welcome to CI/CD E2E Cloud Solution";
     else if (hour < 17 && !(hour == 12))
-    this.message = "Good Afternoon!";
+    this.message = "Good Afternoon! \n\n Welcome to CI/CD E2E Cloud Solution";
     else if (hour == 12)
-    this.message = "Good Noon!";
+    this.message = "Good Noon! \n\n Welcome to CI/CD E2E Cloud Solution";
     else
-    this.message = "Good Evening!";
+    this.message = "Good Evening! \n\n Welcome to CI/CD E2E Cloud Solution";
    	
     assertEquals(message,var.printMessage());
 
